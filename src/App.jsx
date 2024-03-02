6014cf3ea9c759f69be728c458f9b2e3a32c19d4
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-03-02 15:37:49 +0530

    Finalized Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import NewsItems from './components/NewsItems'
 import Category from './components/Category'
-import Form from './components/Form'
+
 
 
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <Navbar/>
-    <Form/>
+   
     <Category/>
     <h3 className='text-center mt-3' style={{ wordSpacing: '.5rem' }}>NewziFy - Top HeadLines Of The Day</h3>
 
