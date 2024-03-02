fc3464a83c289babd40bbe3cad29771d2ceae2b3
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-03-02 12:57:18 +0530

    News Content Fetched Successfully Through API

--- src/components/NewsCards.jsx
+++ src/components/NewsCards.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-function NewsCards() {
+function NewsCards(props) {
+    let {sourceName,newsUrl,imageUrl,title,descprition}=props
   return (
      <div className="card" style={{width:'20rem'}}>
-    <img src="..." className="card-img-top" alt="..."/>
+    <img src={imageUrl} className="card-img-top" alt="..."/>
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="btn btn-sm btn-primary">Read More</a>
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{sourceName}</p>
+      <a href={newsUrl} target='_blank'className="btn btn-sm btn-primary">Read More</a>
     </div>
   </div> 
   )
--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -1,30 +1,42 @@
-import React, { useState ,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import NewsCards from './NewsCards'
 
-function NewsItems() {
+function NewsItems(props) {
     let [articles, setArticles] = useState([]);
     let resultNews = async () => {
-        const url = '';
+        const url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a63ab02946b640f1a45559f967c8e017';
         let data = await fetch(url);
-        // let parsedData = await data.json();
-        // setArticles(parsedData.articles);
+        let parsedData = await data.json();
+        setArticles(parsedData.articles);
 
     };
     useEffect(() => {
         resultNews();
     }, []);
-    
+
     return (
         <>
             <div style={{ margin: '2rem', padding: '.5rem', border: '2px solid black' }}>
                 <div className="row ">
-                    
-                    <div className="col-md-3 mb-3">
+                    {articles.map((element) => {
+                        return (
+                            <div className='col-md-3 mb-3' key={element.url}>
+                                <NewsCards imageURL=
+                                    {element.urlToImage} title={element.title}
+                                    newsUrl={element.url} />
+                            </div>
+                        )
+                    }
 
-                    </div>
+                    )}
+
+
+
 
                 </div>
+
             </div>
+
         </>
     )
 }
