f21b0824dcfc6f9f49d90c53fae47b9d36fd3e1a
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-03-07 23:32:36 +0530

    Unused Code Removed

--- src/components/NewsCards.jsx
+++ src/components/NewsCards.jsx
@@ -1,20 +1,4 @@
-// import React from 'react';
 
-// function NewsCards(props) {
-//   let { sourceName, newsUrl, imageUrl, title,description } = props;
-//   return (
-//     <div className="card" style={{ width: '20rem', height: '27rem' }}>
-//       <img src={imageUrl} style={{ maxHeight: '15rem' }} className="card-img-top" alt="image" />
-//       <div className="card-body" style={{ display: 'flex', flexDirection: 'column' }}>
-//         <h5 className="card-title">{title}</h5>
-//         <p className="card-text flex-grow-1">Source : {sourceName}</p>
-//         <Link href={newsUrl} style={{maxWidth:'8rem'}} target='_blank' className="btn btn-sm btn-primary">Read More</Link>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default NewsCards;
 import React from 'react';
 import {Link} from 'react-router-dom'
 import backgroundImage from '/Users/mangeshyadav/Desktop/React/new-app/src/assets/beams-basic.png'
