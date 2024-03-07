22dd5a167f04df704cf98fc5e0a4dc5150d182a4
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-03-07 20:20:25 +0530

    Profile Detail Customized

--- src/components/ProfileDetail.jsx
+++ src/components/ProfileDetail.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ProfileDetail = (props) => {
   return (
-    <div className="card">
-      <div className="card-header">
+    <div className="card mx-auto mt-5" style={{ maxWidth: "600px" }}>
+      <div className="card-header text-center">
         <h5 className="card-title">Profile Details</h5>
       </div>
       <div className="card-body">
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-md-4 text-md-right">
             <strong>Name:</strong>
           </div>
           <div className="col-md-8">
@@ -16,11 +16,19 @@ const ProfileDetail = (props) => {
           </div>
         </div>
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-md-4 text-md-right">
             <strong>Email:</strong>
           </div>
           <div className="col-md-8">
             {props.userDetails.email}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4 text-md-right">
+            <strong>Account Created On:</strong>
+          </div>
+          <div className="col-md-8">
+            {props.userDetails.created_at}
           </div>
         </div>
         
