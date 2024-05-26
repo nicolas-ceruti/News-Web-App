8abda728d054fd9032b6c75ee2595d877e7a51c3
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-05-26 21:08:45 +0530

    Minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     postcss: {},
-//   },
-// })
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
